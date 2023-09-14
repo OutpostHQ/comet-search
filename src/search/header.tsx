38b8d828df68df23ef580bc5501a2eb5065c96aa
taranbeer <staranbeer@gmail.com>
2023-09-14 14:47:19 +0530

    integrate the search header

--- src/search/header.tsx
+++ src/search/header.tsx
@@ -2,45 +2,56 @@ import React from "react";
 import { RefreshIcon, SearchIcon } from "../icons";
 
 export function Header(props: {
+  question: string;
   mode: "search" | "ask";
   isFirstQuestion?: boolean;
-  setMode: React.Dispatch<React.SetStateAction<"search" | "ask">>;
-  search: React.FormEventHandler<HTMLFormElement>;
+  search: (input: string) => Promise<void>;
   resetSession: () => void;
+  switchMode: () => void;
+  setQuestion: React.Dispatch<React.SetStateAction<string>>;
 }) {
   return (
     <div className="header">
-      {props?.isFirstQuestion ? (
+      {props.isFirstQuestion ? (
         <>
           <SearchIcon className="icon" />
-          <form onSubmit={props.search}>
-            <input placeholder="Search..." autoFocus />
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (props.question.trim()) {
+                console.log(props.question);
+                props.search(props.question);
+              }
+            }}
+          >
+            <input
+              onChange={(e) => {
+                props.setQuestion(e.target.value);
+              }}
+              value={props.question}
+              placeholder="Search..."
+              autoFocus
+            />
           </form>
         </>
       ) : (
-        <>
-          <h3 className="title">Outpost.Ai</h3>
-        </>
+        <h3 className="title">Outpost.Ai</h3>
       )}
       <div className="controls">
-        {!props?.isFirstQuestion && (
+        {!props.isFirstQuestion && (
           <button className="refresh" onClick={props.resetSession}>
             <RefreshIcon className="icon" />
           </button>
         )}
         <div className="mode">
           <button
-            onClick={() => {
-              props?.setMode("search");
-            }}
+            onClick={props.switchMode}
             className={`${props?.mode == "search" && "active"}`}
           >
             Search
           </button>
           <button
-            onClick={() => {
-              props?.setMode("ask");
-            }}
+            onClick={props.switchMode}
             className={`${props?.mode == "ask" && "active"}`}
           >
             ask
